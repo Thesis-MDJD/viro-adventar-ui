0713692cfd78a6fdcbd163efe0953f1ce94dadeb
Mark Khuzam <khuzammark@gmail.com>
2018-05-05 11:21:21 -0700

    first commit/init app and dotenv added

---
+++ AdventaR/index.js
@@ -0,0 +1,16 @@
+import { AppRegistry } from "react-native";
+import App from "./App.js";
+
+if (__DEV__) {
+  require("react-devtools");
+}
+import { YellowBox } from "react-native";
+YellowBox.ignoreWarnings([
+  "Warning: isMounted(...) is deprecated",
+  "Module RCTImageLoader"
+]);
+
+AppRegistry.registerComponent("AdventaR", () => App);
+
+// The below line is necessary for use with the TestBed App
+AppRegistry.registerComponent("ViroSample", () => App);

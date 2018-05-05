e5417fe87a4299cf3246f61b132bd3b93eb2cea9
Mark Khuzam <khuzammark@gmail.com>
2018-05-05 13:03:31 -0700

    added AR Objects from tutorial

--- AdventaR/App.js
+++ AdventaR/App.js
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) 2017-present, Viro, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
 import React, { Component } from "react";
 import {
   AppRegistry,
@@ -16,6 +7,7 @@ import {
   PixelRatio,
   TouchableHighlight
 } from "react-native";
+import { VIRO_KEY } from "react-native-dotenv";
 
 import { ViroVRSceneNavigator, ViroARSceneNavigator } from "react-viro";
 
@@ -23,7 +15,7 @@ import { ViroVRSceneNavigator, ViroARSceneNavigator } from "react-viro";
  TODO: Insert your API key below
  */
 var sharedProps = {
-  apiKey: process.env.VIRO_KEY
+  apiKey: VIRO_KEY
 };
 
 // Sets the default scene you want for AR and VR
--- AdventaR/js/HelloWorldSceneAR.js
+++ AdventaR/js/HelloWorldSceneAR.js
@@ -1,23 +1,30 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
-} from 'react-viro';
+  ViroBox,
+  ViroMaterials,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroARPlaneSelector,
+  ViroNode,
+  ViroAnimations
+} from "react-viro";
 
 export default class HelloWorldSceneAR extends Component {
-
   constructor() {
     super();
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text: "Initializing AR..."
     };
 
     // bind 'this' to functions
@@ -26,8 +33,40 @@ export default class HelloWorldSceneAR extends Component {
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />
+        <ViroBox
+          position={[0, -0.5, -1]}
+          scale={[0.3, 0.3, 0.1]}
+          materials={["grid"]}
+          animation={{ name: "rotate", run: true, loop: true }}
+        />
+        <ViroAmbientLight color={"#aaaaaa"} />
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={90}
+          direction={[0, -1, -0.2]}
+          position={[0, 3, 1]}
+          color="#ffffff"
+          castsShadow={true}
+        />
+        <ViroNode
+          position={[0, -1, 0]}
+          dragType="FixedToWorld"
+          onDrag={() => {}}
+        >
+          <Viro3DObject
+            source={require("./res/emoji_smile/emoji_smile.vrx")}
+            position={[-0.5, 0.5, -1]}
+            scale={[0.2, 0.2, 0.2]}
+            type="VRX"
+          />
+        </ViroNode>
       </ViroARScene>
     );
   }
@@ -35,7 +74,7 @@ export default class HelloWorldSceneAR extends Component {
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello World!"
+        text: "Hello World!"
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
@@ -45,12 +84,27 @@ export default class HelloWorldSceneAR extends Component {
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
-    fontFamily: 'Arial',
+    fontFamily: "Arial",
     fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
-  },
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center"
+  }
+});
+
+ViroMaterials.createMaterials({
+  grid: {
+    diffuseTexture: require("./res/grid_bg.jpg")
+  }
+});
+
+ViroAnimations.registerAnimations({
+  rotate: {
+    properties: {
+      rotateY: "+=90"
+    },
+    duration: 250 //.25 seconds
+  }
 });
 
 module.exports = HelloWorldSceneAR;

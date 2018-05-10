ad04feb0fb76e8b764cdcac30b11ca682d746f13
Mark Khuzam <khuzammark@gmail.com>
2018-05-10 16:40:14 -0700

    firebase returns values

--- AdventaR/app/components/FavoritePlaces.js
+++ AdventaR/app/components/FavoritePlaces.js
@@ -1,12 +1,57 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import { firebaseApp } from "./FireBase";
 
 export default class FavoritePlaces extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: this.props.username,
+      places: []
+    };
+    this.rootRef = firebaseApp
+      .database()
+      .ref()
+      .child("Features")
+      .child("Users")
+      .child("Mark")
+      .child("FavoritePlaces");
+  }
+
+  componentDidMount() {
+    const self = this;
+    this.rootRef.on("value", function(snapshot) {
+      const values = [];
+      for (let key in snapshot.val()) {
+        alert(JSON.stringify(snapshot.val()[key]));
+        values.push(snapshot.val()[key]);
+      }
+      // alert(JSON.stringify(snapshot));
+      self.setState({
+        places: values
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.rootRef.off();
+  }
   render() {
     return (
       <View>
-        <Text> Favorite Places </Text>
+        {this.state.places.map(place => {
+          return (
+            <View key={place.yelpId}>
+              <Text> {place.Name} </Text>
+              <Text> {place.Rating} </Text>
+            </View>
+          );
+        })}
       </View>
     );
   }
 }
+
+// See what my favorite places are
+
+// - Select favorite places from users where user is username
--- AdventaR/app/components/FireBase.js
+++ AdventaR/app/components/FireBase.js
@@ -14,3 +14,32 @@ const firebaseConfig = {
 };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+/* 
+
+See who my friends are
+
+- Select * from friends where user1 === username || user2 === username
+
+
+
+Check my visit history
+
+- Select visit history from users where user is username
+
+See what chat rooms im in
+
+- Select * from conversations where im one of the users
+
+See the messages inside that chatroom
+
+- Select * from messages order by conversation = conversation.id
+
+request friends and accept friends
+
+- Select * from requests where user 1 or user 2 === my user.id
+
+- Select * from friends where user 1 or user 2 === my user.id
+
+
+*/

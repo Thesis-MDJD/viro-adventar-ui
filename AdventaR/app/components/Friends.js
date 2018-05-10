fd8bc2a3e45123e5204b7c0c5835735ea6e1cca5
Mark Khuzam <khuzammark@gmail.com>
2018-05-10 16:40:14 -0700

    firebase returns values

--- AdventaR/app/components/Friends.js
+++ AdventaR/app/components/Friends.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import { Text, View } from "react-native";
 import { firebaseApp } from "./FireBase";
 
+const rootRef = firebaseApp.database().ref();
+const itemsRef = rootRef.child("items");
+
 export default class Friends extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      dataSource: []
+    };
   }
   getRef = () => {
     return firebaseApp.database().ref();
@@ -23,10 +28,16 @@ export default class Friends extends Component {
       });
 
       this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(items)
+        dataSource: items
       });
     });
   };
+
+  componentDidMount() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows([{ title: "Pizza" }])
+    });
+  }
 
   render() {
     return (

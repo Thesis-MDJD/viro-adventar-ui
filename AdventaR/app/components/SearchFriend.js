0e06bf7de14cada0ddd8b8464d4cf84bc93bb347
Daniel Lai <dlai813@gmail.com>
2018-05-11 13:07:05 -0700

    in progress SearchFriend

--- AdventaR/app/components/SearchFriend.js
+++ AdventaR/app/components/SearchFriend.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { View, Text, StyleSheet, Image, FlatList} from 'react-native';
 import { SearchBar, ListItem, List } from 'react-native-elements'
 
 =======
+=======
+>>>>>>> in progress SearchFriend
 import { View, ScrollView, Input, Text } from 'react-native';
 import { SearchBar, Avatar } from 'react-native-elements';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 // import { Input } from 'react-native-elements';
+<<<<<<< HEAD
 >>>>>>> in progress SearchFriend
 =======
 import { View, Text, StyleSheet, Image, FlatList} from 'react-native';
 import { SearchBar, ListItem, List } from 'react-native-elements'
 
 >>>>>>> implemented friends list with hard data
+=======
+>>>>>>> in progress SearchFriend
 
 export default class SearchFriend extends Component {
   constructor(props) {
     super(props);
     this.state = {
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
       loading: false,
@@ -42,6 +49,13 @@ export default class SearchFriend extends Component {
 =======
       ],
 >>>>>>> implemented friends list with hard data
+=======
+      searched: [
+        {name: 'Lady1', img: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'},
+        {name: 'Lady2', img: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg'},
+        {name: 'Lady3', img: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'}
+      ]
+>>>>>>> in progress SearchFriend
     }
   }
 
@@ -49,6 +63,7 @@ export default class SearchFriend extends Component {
     //run the search function here
   }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
   renderHeader() {
@@ -131,3 +146,44 @@ const styles = StyleSheet.create({
 =======
 })
 >>>>>>> implemented friends list with hard data
+=======
+
+  render() {
+    const searchedFriends =
+      this.state.searched.map( user => {
+        return 
+          <View key={user.name}>
+            <Avatar
+              size='medium'
+              rounded
+              source={{uri: user.img}}
+              activeOpacity={0.7}
+            />
+            <Text>{user.name}</Text>
+          </View>
+      })
+
+    return(
+      <View>
+        {/* <SearchBar
+          lightTheme
+          autoCorrect='none'
+          // autoCapitalize={false} // is our username always Capitalized?
+          placeholder='Search for chat here'
+          onChangeText={(text) => this.onTextInput(text)}
+        /> */}
+        <View key={this.state.searched[0].name}>
+            <Avatar
+              size='medium'
+              rounded
+              source={{uri: this.state.searched[0].img}}
+              activeOpacity={0.7}
+            />
+            <Text>{this.state.searched[0].name}</Text>
+        </View>
+        {searchedFriends}
+      </View>
+    )
+  }
+}
+>>>>>>> in progress SearchFriend

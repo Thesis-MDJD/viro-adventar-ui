22287db346e34933fa6467a26be7199725cf0dd1
Daniel Lai <dlai813@gmail.com>
2018-05-11 20:52:58 -0700

    implemented friends list with hard data

--- AdventaR/app/components/SearchFriend.js
+++ AdventaR/app/components/SearchFriend.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { View, Text, StyleSheet, Image, FlatList} from 'react-native';
 import { SearchBar, ListItem, List } from 'react-native-elements'
 
@@ -9,11 +10,17 @@ import { SearchBar, Avatar } from 'react-native-elements';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 // import { Input } from 'react-native-elements';
 >>>>>>> in progress SearchFriend
+=======
+import { View, Text, StyleSheet, Image, FlatList} from 'react-native';
+import { SearchBar, ListItem, List } from 'react-native-elements'
+
+>>>>>>> implemented friends list with hard data
 
 export default class SearchFriend extends Component {
   constructor(props) {
     super(props);
     this.state = {
+<<<<<<< HEAD
 <<<<<<< HEAD
       loading: false,
       searched: [
@@ -22,12 +29,19 @@ export default class SearchFriend extends Component {
         {name: 'Lady3', img: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'}
       ],
 =======
+=======
+      loading: false,
+>>>>>>> implemented friends list with hard data
       searched: [
-        {name: 'Lady1', img: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'},
+        {name: 'Lady1WOIENW', img: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'},
         {name: 'Lady2', img: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg'},
         {name: 'Lady3', img: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'}
+<<<<<<< HEAD
       ]
 >>>>>>> in progress SearchFriend
+=======
+      ],
+>>>>>>> implemented friends list with hard data
     }
   }
 
@@ -35,6 +49,7 @@ export default class SearchFriend extends Component {
     //run the search function here
   }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
   renderHeader() {
     return <SearchBar placeholder='Search for friends here...' lightTheme round />
@@ -73,43 +88,46 @@ const styles = StyleSheet.create({
   }
 })
 =======
+=======
+  renderHeader() {
+    return <SearchBar placeholder='Search for friends here...' lightTheme round />
+  }
+>>>>>>> implemented friends list with hard data
 
   render() {
-    const searchedFriends =
-      this.state.searched.map( user => {
-        return 
-          <View key={user.name}>
-            <Avatar
-              size='medium'
-              rounded
-              source={{uri: user.img}}
-              activeOpacity={0.7}
-            />
-            <Text>{user.name}</Text>
-          </View>
-      })
-
     return(
-      <View>
-        {/* <SearchBar
-          lightTheme
-          autoCorrect='none'
-          // autoCapitalize={false} // is our username always Capitalized?
-          placeholder='Search for chat here'
-          onChangeText={(text) => this.onTextInput(text)}
-        /> */}
-        <View key={this.state.searched[0].name}>
-            <Avatar
-              size='medium'
-              rounded
-              source={{uri: this.state.searched[0].img}}
-              activeOpacity={0.7}
-            />
-            <Text>{this.state.searched[0].name}</Text>
-        </View>
-        {searchedFriends}
-      </View>
-    )
+      <List style={styles.container}>
+        <FlatList
+          data={this.state.searched}
+          ListHeaderComponent={this.renderHeader}
+          renderItem={({item}) => {
+            return (
+              <ListItem
+              roundAvatar
+              title={item.name}
+              avatar={{ uri: item.img }}
+              />
+            )
+          }}
+          keyExtractor={item => item.img} // change to key later
+          />
+      </List>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  flatListFooter: {
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: 'grey'
+  }
+<<<<<<< HEAD
+}
 >>>>>>> in progress SearchFriend
+=======
+})
+>>>>>>> implemented friends list with hard data

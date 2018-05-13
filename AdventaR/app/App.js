9a88136adccd3a75d9e58ac866efb7adc2235171
Daniel Lai <dlai813@gmail.com>
2018-05-12 17:07:36 -0700

    rebase attempt 1

--- AdventaR/app/App.js
+++ AdventaR/app/App.js
@@ -1,14 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
-import {
-  StackNavigator,
-  SwitchNavigator,
-  TabNavigator
-} from "react-navigation";
-=======
 import { StackNavigator, SwitchNavigator, TabNavigator } from "react-navigation";
 import Ionicons from 'react-native-vector-icons/Ionicons';
->>>>>>> implemented navbar with friends and convo
 import Restaurants from "./components/Restaurants";
 import Camera from "./components/Camera";
 import LoginScreen from "./components/LoginScreen";

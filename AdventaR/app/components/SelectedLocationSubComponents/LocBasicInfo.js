525bac27b68e6c774a9929c614bdb63d8474ad9e
Daniel Lai <dlai813@gmail.com>
2018-05-18 19:50:20 -0700

    added new markers

--- AdventaR/app/components/SelectedLocationSubComponents/LocBasicInfo.js
+++ AdventaR/app/components/SelectedLocationSubComponents/LocBasicInfo.js
@@ -1,65 +1,224 @@
-import React, { Component } from "react";
-import { StyleSheet, View, Text, } from "react-native";
-import { Icon } from "react-native-elements";
-
-import LocRating from "./LocRating";
-import LocPriceRange from "./LocPriceRange";
-import LocHours from "./LocHours";
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, AsyncStorage} from 'react-native';
+import { Icon } from 'react-native-elements';
+import { firebaseApp } from '../FireBase';
+import LocRating from './LocRating';
+import LocPriceRange from './LocPriceRange';
+import LocHours from './LocHours';
 
 export default class LocBasicInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "0", //String
-      reviewCount: 0, //integer
+      name: '0', //String
+      review_count: 0, //integer
       categories: [],
       // non-yelp
-      favorite: false 
+      favorite: false,
+      checkedIn: false
     };
-    this.onFavoritePress = this.onFavoritePress.bind(this);
+    this.rootRef = firebaseApp
+      .database()
+      .ref()
+      .child("Features");
   }
 
-  onFavoritePress() {
-    this.setState({favorite: !this.state.favorite});
-    // send info to user data base
+  componentDidMount() {
+    this.checkFavoriteStatus(this.props.yelpId);
+    this.checkCheckedInStatus(this.props.yelpId);
+  }
+
+  onCheckedInPress = async () => {
+    try {
+      const userId = await AsyncStorage.getItem('dbId');
+      this.state.checkedIn ?
+      this.removeCheckedInPlace(this.props.yelpId)
+      :
+      this.addCheckedInPlace(userId, this.props.name, this.props.rating, this.props.photo, this.props.yelpId);
+        
+      this.props.updateCheckedIn(this.props.yelpId)
+    } catch (error) {
+      console.log('Error on adding/removing checkedIn', error);
+    }
+  }
+
+  checkCheckedInStatus = async (yelpId) => {
+    try {
+      const userId = await AsyncStorage.getItem('dbId');
+      let status = this.rootRef
+        .child('Users')
+        .child(userId)
+        .child('CheckedInPlaces')
+        .orderByChild('yelpId')
+        .equalTo(yelpId)
+      status.once('value')
+        .then( snapshot => {
+          snapshot.exists() && this.setState({checkedIn: true})
+        })
+    } catch (error) {
+      console.log('Error at check', error)
+    }
+  }
+
+  addCheckedInPlace = (userId, name, rating, image, yelpId) => {
+    const place = {
+      name,
+      image,
+      rating,
+      yelpId
+    };
+    this.rootRef
+      .child("Users")
+      .child(userId)
+      .child("CheckedInPlaces")
+      .push()
+      .set(place);
+    this.setState({checkedIn: true})
+  };
+
+  removeCheckedInPlace = async (yelpId) => {
+    try {
+      const userId = await AsyncStorage.getItem('dbId');
+      let checkedIned = this.rootRef
+        .child('Users')
+        .child(userId)
+        .child('CheckedInPlaces')
+        .orderByChild('yelpId')
+        .equalTo(yelpId)
+      checkedIned.once('value', (snapshot) => {
+        let fbId = Object.keys(snapshot.val())
+        this.rootRef
+        .child('Users')
+        .child(userId)
+        .child('CheckedInPlaces')
+        .child(fbId[0])
+        .ref.remove()
+      });
+      this.setState({checkedIn: false});
+    } catch (error) {
+      console.log('Error', error)
+    }
+  }
+
+  onFavoritePress = async () => {
+    try {
+      const userId = await AsyncStorage.getItem('dbId');
+      this.state.favorite ?
+      this.removeFavoritePlace(this.props.yelpId)
+      :
+      this.addFavoritePlace(userId, this.props.name, this.props.rating, this.props.photo, this.props.yelpId);
+        
+      this.props.updateFavorite(this.props.yelpId)
+    } catch (error) {
+      console.log('Error on adding/removing Favoriting', error);
+    }
+  }
+
+  checkFavoriteStatus = async (yelpId) => {
+    try {
+      const userId = await AsyncStorage.getItem('dbId');
+      let status = this.rootRef
+        .child('Users')
+        .child(userId)
+        .child('FavoritePlaces')
+        .orderByChild('yelpId')
+        .equalTo(yelpId)
+      status.once('value')
+        .then( snapshot => {
+          snapshot.exists() && this.setState({favorite: true})
+        })
+    } catch (error) {
+      console.log('Error at check', error)
+    }
+  }
+
+  addFavoritePlace = (userId, name, rating, image, yelpId) => {
+    const place = {
+      name,
+      image,
+      rating,
+      yelpId
+    };
+    this.rootRef
+      .child("Users")
+      .child(userId)
+      .child("FavoritePlaces")
+      .push()
+      .set(place);
+    this.setState({favorite: true})
+  };
+
+  removeFavoritePlace = async (yelpId) => {
+    try {
+      const userId = await AsyncStorage.getItem('dbId');
+      let favorited = this.rootRef
+        .child('Users')
+        .child(userId)
+        .child('FavoritePlaces')
+        .orderByChild('yelpId')
+        .equalTo(yelpId)
+      favorited.once('value', (snapshot) => {
+        let fbId = Object.keys(snapshot.val())
+        this.rootRef
+        .child('Users')
+        .child(userId)
+        .child('FavoritePlaces')
+        .child(fbId[0])
+        .ref.remove()
+      });
+      this.setState({favorite: false});
+    } catch (error) {
+      console.log('Error', error)
+    }
   }
 
   render() {
+    const checkedInStatus = this.state.checkedIn ?
+      <View>
+        <Icon name='check-circle' type='material-community' color='#1aa85d' onPress={this.onCheckedInPress}/>
+      </View>
+      :
+      <View>
+        <Icon name='check-circle-outline' type='material-community' color='#769db0' onPress={this.onCheckedInPress}/>
+      </View>
+
     const favoriteStatus = this.state.favorite ?
-    //Favorited Heart
       <View>
         <Icon name='heart' type='material-community' color='#ff4f7d' onPress={this.onFavoritePress}/>
       </View>
       :
-    // Unfavorited Heart
       <View>
         <Icon name='heart-outline' type='material-community' color='#769db0' onPress={this.onFavoritePress}/>
-      </View>;
+      </View>
 
-    const categories = this.props.categories.map( category => category.title ).join(", ");
+    const categories = this.props.categories.map( category => category.title ).join(', ');
 
-    return (
+    return(
       <View style={styles.container}>
 
         <View style={styles.nameFavContainer}>
-          <Text style={styles.name} /* numberOfLines= {1} ellipsizeMode='tail'*/> {this.props.name}</Text>
+          <Text style={styles.name}> {this.props.name}</Text>        
+          {checkedInStatus}
           {favoriteStatus}
         </View>
 
         <View style={styles.ratingReviewContainer}>
           <LocRating rating={this.props.rating}/>
-          <Text > base on {this.props.reviewCount} Reviews </Text>
+          <Text> base on {this.props.review_count} Reviews </Text>
         </View>
 
-        <View style={styles.priceCategoryContainer}>
+        <View style={styles.priceContainer}>
           <LocPriceRange price={this.props.price}/>
-          <Icon name='dot-single' type='entypo' color='#999999' />
-          <Text>{categories}</Text>
+        </View>
+
+        <View style={styles.categoriesContainer}>
+          <Text ellipsizeMode='tail' >{categories}</Text>
         </View>
 
         <LocHours hours={this.props.hours}/>
+
       </View>
-    );
+    )
   }
 }
 
@@ -67,40 +226,49 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
     flex: 1,
-    backgroundColor: "#f5f5f5",
+    backgroundColor: '#f5f5f5',
     paddingVertical: 15,
     marginVertical: 5
   },
 
   nameFavContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center"
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginVertical: 2.5
   },
 
   name: {
-    fontWeight: "bold"
+    fontSize: 18,
+    fontWeight: 'bold'
   },
 
   ratingReviewContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center"
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 2.5
   },
 
-  priceCategoryContainer: {
+  priceContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center"
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginVertical: 2.5
   },
 
-  hoursContainer: {
+  categoriesContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginVertical: 2.5
   },
 
   hours: {
-    color: "red"
+    color: 'red'
   }
-});
+})
--- AdventaR/app/components/SelectedLocationSubComponents/LocHours.js
+++ AdventaR/app/components/SelectedLocationSubComponents/LocHours.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import {Text, StyleSheet, View } from "react-native";
-const moment = require("moment");
+import React, { Component } from 'react';
+import {Text, StyleSheet, View } from 'react-native'
+const moment = require('moment');
 
 export default class LocHours extends Component {
   constructor(props) {
@@ -8,54 +8,55 @@ export default class LocHours extends Component {
     this.state = {
       hours: [
         {
-          open: [
-            {
-              "is_overnight": Boolean, //Boolean, Whether the business opens overnight or not. When this is true, the end time will be lower than the start time.
-              "start": "", //String, Start of the opening hours in a day, in 24-hour clock notation, like 1000 means 10 AM.
-              "end": "", //String, End of the opening hours in a day, in 24-hour clock notation, like 2130 means 9:30 PM.
-              "day": 0 //Int, From 0 to 6, representing day of the week from Monday to Sunday. Notice that you may get the same day of the week more than once if the business has more than one opening time slots.
-            }
-          ],
-          "hours_type": "", //String, The type of the opening hours information. Right now, this is always REGULAR.
-          "is_open_now": Boolean //Boolean, Describe is business is open now
+            open: [
+                {
+                    "is_overnight": Boolean, //Boolean, Whether the business opens overnight or not. When this is true, the end time will be lower than the start time.
+                    "start": "", //String, Start of the opening hours in a day, in 24-hour clock notation, like 1000 means 10 AM.
+                    "end": "", //String, End of the opening hours in a day, in 24-hour clock notation, like 2130 means 9:30 PM.
+                    "day": 0 //Int, From 0 to 6, representing day of the week from Monday to Sunday. Notice that you may get the same day of the week more than once if the business has more than one opening time slots.
+                }
+            ],
+            "hours_type": "", //String, The type of the opening hours information. Right now, this is always REGULAR.
+            "is_open_now": Boolean //Boolean, Describe is business is open now
         }
       ]
-    };
+    }
   }
 
   render() {
     const openStatus = this.props.hours ?
       this.props.hours[0].is_open_now ?
-        <Text style={{color: "#79C727"}}> Open </Text>
+        <Text style={{color: '#79C727'}}> Open </Text>
         :
-        <Text style={{color: "#C41200"}}> Closed </Text>
-      : null;
+        <Text style={{color: '#C41200'}}> Closed </Text>
+      : null
 
-    const todayDayOfWeek = moment().diff(moment().startOf("week"), "days");
+    const todayDayOfWeek = moment().diff(moment().startOf('week'), 'days')
 
     const todayHour = this.props.hours && this.props.hours[0].open.map( day => {
       return (
         day.day === todayDayOfWeek ?
-          `${moment(day.start, "hmm").format("hh:mm A")} - ${moment(day.end, "hmm").format("hh:mm A")}`
-          :
-          null
-      );
-    });
+        `${moment(day.start, 'hmm').format('hh:mm A')} - ${moment(day.end, 'hmm').format('hh:mm A')}`
+        :
+        null
+      )
+    })
 
-    return (
+    return(
       <View style={styles.container}>
         {openStatus}
         <Text>{todayHour}</Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center"
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginVertical: 2.5
   }
-});
+})

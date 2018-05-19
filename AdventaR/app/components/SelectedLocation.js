b2b79e1f689d918e6433687e144c82f01f1dafaf
Daniel Lai <dlai813@gmail.com>
2018-05-18 21:35:59 -0700

    updated marker logic

--- AdventaR/app/components/SelectedLocation.js
+++ AdventaR/app/components/SelectedLocation.js
@@ -1,105 +1,146 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { 
   View,
+  Text,
   ScrollView,
+  Image,
   Button,
   StyleSheet,
   StatusBar
-} from "react-native";
-import LocPics from "./SelectedLocationSubComponents/LocPics";
-import LocBasicInfo from "./SelectedLocationSubComponents/LocBasicInfo";
-import LocContactInfo from "./SelectedLocationSubComponents/LocContactInfo";
+ } from 'react-native';
+import LocPics from './SelectedLocationSubComponents/LocPics';
+import LocBasicInfo from './SelectedLocationSubComponents/LocBasicInfo';
+import LocContactInfo from './SelectedLocationSubComponents/LocContactInfo';
 import { REST_SERVER_IP } from "react-native-dotenv";
 
 
-export default class SelectedLocation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      data: {},
-      restaurantId: this.props.navigation.state.params.restaurantId
-    };
-  }
-
-
-   getPlace = async (id) => {
-     try {
-       const data = await fetch(
-         `http://${REST_SERVER_IP}/yelp/business/${id}`,
-       );
-       const result = await data.json();
-       this.setState({ data: result });
-     } catch (error) {
-       console.log("Fetch Error = ", error);
+ export default class SelectedLocation extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       name: '',
+       data: {},
+       restaurantId: this.props.navigation.state.params.restaurantId,
+       distance: this.props.navigation.state.params.distance,
+       updateFavoritedLocations: this.props.navigation.state.params.updateFavoritedLocations,
+       updateCheckedInLocations: this.props.navigation.state.params.updateCheckedInLocations,
+      tabbed: this.props.navigation.state.params.tabbed
      }
    };
 
+
+   getPlace = async (id) => {
+    try {
+      const data = await fetch(
+        `http://${REST_SERVER_IP}/yelp/business/${id}`,
+      );
+      const result = await data.json();
+      this.setState({ data: result })
+    } catch (error) {
+      console.log("Fetch Error = ", error);
+    }
+  };
+
    componentDidMount() {
-     this.getPlace(this.state.restaurantId);
-     this.props.navigation.setParams({ goBackToPrevious: this.goBackToPrevious});
+    this.getPlace(this.state.restaurantId)
    }
 
    static navigationOptions = ({ navigation }) => {
-     const params = navigation.state.params || {};
-     return {
-       // selected location's name
-       title: "Selected Location",
-       headerStyle: {
-         backgroundColor: "#f4511e"
-       },
-       headerTintColor: "#fff",
-       headerTitleStyle: {
-         fontWeight: "bold"
-       },
-       // go back to previous view
-       headerLeft: (
-         <Button onPress={params.goBackToPrevious} title="Back" color="#fff" />
-       )
-     };
-   };
-
-  goBackToPrevious = () => {
-    this.props.navigation.goBack();
+    const params = navigation.state.params || {};
+    return {
+      title: params.name,
+      headerStyle: {
+        backgroundColor: "#f4511e"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold"
+      },
+      headerLeft: (
+        <Button onPress={() => navigation.goBack()} title="Back" color="#fff" />
+      )
+    };
   };
 
   render() {
     const info = this.state.data;
+    const distance = this.state.distance === null ?
+      null
+      : (this.state.distance < 321.869) ?
+        <Text style={styles.distanceText} color='#888882'>{Math.floor(this.state.distance * 3.2808399)} ft away</Text>
+        :
+        <Text style={styles.distanceText} color='#888882'>{Math.floor(this.state.distance * 0.000621371 * 10) / 10} mi away</Text>;
 
-    return (
-      <ScrollView>
+    const address = info.location && info.location.display_address.join(' ');
+    
+    return(
+      <ScrollView contentContainerStyle={{
+        flexGrow: 1,
+        justifyContent: 'space-between'
+      }}>
         <StatusBar barStyle='light-content' />
-        <View>
-          {info.name ? <LocPics photos={info.photos}/> : null}
-        </View>
+          <View>
+            {info.name ? <LocPics photos={info.photos}/> : null}
+          </View>
 
-        <View>
-          {info.name ? 
+          <View>
+            {info.name ? 
             <LocBasicInfo 
               name={info.name} 
-              review_count={info.reviewCount}
+              review_count={info.review_count} 
               categories={info.categories} 
               favorite={info.favorite}
               rating={info.rating}
               price={info.price}
               hours={info.hours}
-            /> : null}          
-        </View>
+              yelpId={info.id}
+              photo={info.photos[0]}
+              updateFavorite={this.state.updateFavoritedLocations}
+              updateCheckedIn={this.state.updateCheckedInLocations}
+              />: null}          
+          </View>
 
-        <View>
-          {info.name ? <LocContactInfo phone={info.phone} display_phone={info.displayPhone} url={info.url}/> : null }
-        </View>
+          <View style={styles.addressContainer}>
+            <View style={styles.address}>
+              <Text style={styles.addressText}>{address}</Text>
+            </View>
+
+            <View style={styles.distance}>
+              {distance}
+            </View>
+          </View>
+
+          <View>
+            {info.name ? <LocContactInfo phone={info.phone} display_phone={info.display_phone} url={info.url}/> : null }
+          </View>
       </ScrollView>
-    );
-  }
+    )
+  };
 
-  
 }
 
 const styles = StyleSheet.create({
-  container: {
+  addressContainer: {
+    backgroundColor: '#fff'
+  },
+  address: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between"
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginTop: 5,
+    marginBottom: 2.5
+  },
+  addressText: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  distance: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginBottom: 5
+  },
+  distanceText: {
+    fontSize: 12
   }
-});
+})
--- AdventaR/app/components/SelectedLocationSubComponents/LocRating.js
+++ AdventaR/app/components/SelectedLocationSubComponents/LocRating.js
@@ -1,91 +1,91 @@
-import React, { Component } from "react";
-import { View, StyleSheet, Image, Platform } from "react-native";
+import React, { Component } from 'react';
+import { View, StyleSheet, Image, Platform } from 'react-native';
 
 export default class LocRating extends Component {
   constructor(props) {
     super(props);
     this.state = {
       rating: 0, //Float: A signed double-precision floating-point value.
-    };
+    }
   }
 
   render() {
     let rating = this.props.rating;
 
     const currentRatingAndroid = rating === 5 ?
-      <Image height={34} width={170} resizeMode='contain' source={{uri: "stars_large_5.png"}} />  
+      <Image height={34} width={170} resizeMode='contain' source={{uri: 'stars_large_5.png'}} />  
       :
       rating === 4.5 ?
-        <Image height={34} width={170} resizeMode='contain' source={{uri: "stars_large_4_half.png"}} /> 
+        <Image height={34} width={170} resizeMode='contain' source={{uri: 'stars_large_4_half.png'}} /> 
         :
         rating === 4 ?
-          <Image height={34} width={170} resizeMode='contain' source={{uri: "stars_large_4.png"}} />
+          <Image height={34} width={170} resizeMode='contain' source={{uri: 'stars_large_4.png'}} />
           :
           rating === 3.5 ?
-            <Image height={34} width={170} resizeMode='contain' source={{uri: "stars_large_3_half.png"}} />
+            <Image height={34} width={170} resizeMode='contain' source={{uri: 'stars_large_3_half.png'}} />
             :
             rating === 3 ?
-              <Image height={34} width={170} resizeMode='contain' source={{uri: "stars_large_3.png"}} />
+              <Image height={34} width={170} resizeMode='contain' source={{uri: 'stars_large_3.png'}} />
               :
               rating === 2.5 ?
-                <Image height={34} width={170} resizeMode='contain' source={{uri: "stars_large_2_half.png"}} />
+                <Image height={34} width={170} resizeMode='contain' source={{uri: 'stars_large_2_half.png'}} />
                 :
                 rating === 2 ?
-                  <Image height={34} width={170} resizeMode='contain' source={{uri: "stars_large_2.png"}} />
+                  <Image height={34} width={170} resizeMode='contain' source={{uri: 'stars_large_2.png'}} />
                   :
                   rating === 1.5 ?
-                    <Image height={34} width={170} resizeMode='contain' source={{uri: "stars_large_1_half.png"}} />
+                      <Image height={34} width={170} resizeMode='contain' source={{uri: 'stars_large_1_half.png'}} />
                     :
                     rating === 1 ?
-                      <Image height={34} width={170} resizeMode='contain' source={{uri: "stars_large_1.png"}} />
+                      <Image height={34} width={170} resizeMode='contain' source={{uri: 'stars_large_1.png'}} />
                       :  
-                      <Image height={34} width={170} resizeMode='contain' source={{uri: "stars_large_0.png"}} />;
+                      <Image height={34} width={170} resizeMode='contain' source={{uri: 'stars_large_0.png'}} />
 
     const currentRatingIOSWEB = rating === 5 ?
-      <Image resizeMode='contain' style={styles.iosImage} source={{uri: "large_5"}} />  
+      <Image resizeMode='contain' style={styles.iosImage} source={{uri: 'small_5'}} />  
       :
       rating === 4.5 ?
-        <Image resizeMode='contain' style={styles.iosImage} source={{uri: "large_4_half"}} /> 
+        <Image resizeMode='contain' style={styles.iosImage} source={{uri: 'small_4_half'}} /> 
         :
         rating === 4 ?
-          <Image resizeMode='contain' style={styles.iosImage} source={{uri: "large_4"}} />
+          <Image resizeMode='contain' style={styles.iosImage} source={{uri: 'small_4'}} />
           :
           rating === 3.5 ?
-            <Image resizeMode='contain' style={styles.iosImage} source={{uri: "large_3_half"}} />
+            <Image resizeMode='contain' style={styles.iosImage} source={{uri: 'small_3_half'}} />
             :
             rating === 3 ?
-              <Image resizeMode='contain' style={styles.iosImage} source={{uri: "large_3"}} />
+              <Image resizeMode='contain' style={styles.iosImage} source={{uri: 'small_3'}} />
               :
               rating === 2.5 ?
-                <Image resizeMode='contain' style={styles.iosImage} source={{uri: "large_2_half"}} />
+                <Image resizeMode='contain' style={styles.iosImage} source={{uri: 'small_2_half'}} />
                 :
                 rating === 2 ?
-                  <Image resizeMode='contain' style={styles.iosImage} source={{uri: "large_2"}} />
+                  <Image resizeMode='contain' style={styles.iosImage} source={{uri: 'small_2'}} />
                   :
                   rating === 1.5 ?
-                    <Image resizeMode='contain' style={styles.iosImage} source={{uri: "large_1_half"}} />
+                      <Image resizeMode='contain' style={styles.iosImage} source={{uri: 'small_1_half'}} />
                     :
                     rating === 1 ?
-                      <Image resizeMode='contain' style={styles.iosImage} source={{uri: "large_1"}} />
+                      <Image resizeMode='contain' style={styles.iosImage} source={{uri: 'small_1'}} />
                       :  
-                      <Image resizeMode='contain' style={styles.iosImage} source={{uri: "large_0"}} />;
+                      <Image resizeMode='contain' style={styles.iosImage} source={{uri: 'small_0'}} />
 
-    let currentRating = Platform.OS === "android" ?
+    let currentRating = Platform.OS === 'android' ?
       currentRatingAndroid
       :
-      currentRatingIOSWEB;
+      currentRatingIOSWEB
 
-    return (
+    return(
       <View style={styles.container}>
         {currentRating}
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   iosImage: {
     height: 34,
-    width: 170
+     width: 170
   }
-});
+})
